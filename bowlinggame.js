918abf52d13a2ab6f18042a0f09275a5b164c559
= <=>
2017-02-18 14:41:49 +0100

    added count of every frame pin drop (no strikes, no spare)

--- bowlinggame.js
+++ bowlinggame.js
@@ -1,11 +1,21 @@
 function Roll(firstStrike, secondStrike){
-
+    if (this.Frames == undefined)
+        this.Frames = [];
+    this.Frames.push({firstRoll: firstStrike, secondRoll: secondStrike});
 }
 function GetScore(){
-    return 0;
+    var score = 0;
+    this.Frames.forEach(function(element) {
+        score = parseInt(score) + parseInt(element.firstRoll) + parseInt(element.secondRoll);
+    }, this);
+    return score;
+}
+function Initialize(){
+    this.Frames = undefined;
 }
 
 module.exports = {
     Roll: Roll,
-    GetScore: GetScore
+    GetScore: GetScore,
+    Initialize: Initialize
 }
--- test/bowlingtest.js
+++ test/bowlingtest.js
@@ -3,9 +3,23 @@ var BowlingGame = require('../bowlinggame');
 
 describe('BowlingGame', function() {
   describe('#add()', function() {
-    it('should do nothing if nothing done', function() {
-        BowlingGame.Roll(0,0)
+    it('should return score of zero if nothing was striked', function() {
+        BowlingGame.Initialize();
+        for (i = 0; i < 10; i++){
+            BowlingGame.Roll(0,0)
+        }
         assert.equal(0, BowlingGame.GetScore());
+    });
+  });
+});
+describe('BowlingGame', function() {
+  describe('#add()', function() {
+    it('should return score of 10 if one pin striked each frame', function() {
+        BowlingGame.Initialize();        
+        for (i = 0; i < 10; i++){
+            BowlingGame.Roll(1,0)
+        }
+        assert.equal(10, BowlingGame.GetScore());
     });
   });
 });
