943f42c39f19490f17659479105a64ba03a45230
= <=>
2017-02-18 19:29:12 +0100

    strike and spare handled (except on the last frame)

--- bowlinggame.js
+++ bowlinggame.js
@@ -1,16 +1,48 @@
-function Roll(firstStrike, secondStrike){
+function Roll(firstStrike, secondStrike) {
     if (this.Frames == undefined)
         this.Frames = [];
-    this.Frames.push({firstRoll: firstStrike, secondRoll: secondStrike});
+    var isSpare = IsSpare(firstStrike, secondStrike);
+    var isStrike = IsStrike(firstStrike);
+    this.Frames.push({ firstRoll: firstStrike, secondRoll: secondStrike, isSpare: isSpare, isStrike: isStrike });
 }
-function GetScore(){
+function IsSpare(firstStrike, secondStrike) {
+    firstStrike = parseInt(firstStrike);
+    secondStrike = parseInt(secondStrike);
+    if (firstStrike != 10 && firstStrike + secondStrike == 10)
+        return true;
+    return false;
+}
+function IsStrike(firstStrike) {
+    if (firstStrike == 10)
+        return true;
+    return false;
+}
+function GetScore() {
     var score = 0;
-    this.Frames.forEach(function(element) {
-        score = parseInt(score) + parseInt(element.firstRoll) + parseInt(element.secondRoll);
+    for (i = 0; i < this.Frames.length; i++) {
+        var currentFrame = this.Frames[i];
+        var bonusPoints = GetBonusPoints(this.Frames[i], this.Frames[i + 1], this.Frames[i + 2])
+        score = parseInt(score) + parseInt(currentFrame.firstRoll) + parseInt(currentFrame.secondRoll) + bonusPoints;
+    }
+    this.Frames.forEach(function (element) {
     }, this);
     return score;
 }
-function Initialize(){
+function GetBonusPoints(currentFrame, nextFrame, secondNextFrame) {
+    var bonusPoints = 0;
+    if (currentFrame.isSpare) {
+        bonusPoints = nextFrame.firstRoll;
+    }
+    if (currentFrame.isStrike && !nextFrame.isStrike){
+        bonusPoints = nextFrame.firstRoll + nextFrame.secondRoll;
+    }
+    if (currentFrame.isStrike && nextFrame.isStrike){
+        bonusPoints = nextFrame.firstRoll + secondNextFrame.firstRoll;
+    }
+    return bonusPoints;
+
+}
+function Initialize() {
     this.Frames = undefined;
 }
 
--- test/bowlingtest.js
+++ test/bowlingtest.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 var BowlingGame = require('../bowlinggame');
 
 describe('BowlingGame', function() {
-  describe('#add()', function() {
+  describe('#Roll()', function() {
     it('should return score of zero if nothing was striked', function() {
         BowlingGame.Initialize();
         for (i = 0; i < 10; i++){
@@ -13,13 +13,56 @@ describe('BowlingGame', function() {
   });
 });
 describe('BowlingGame', function() {
-  describe('#add()', function() {
+  describe('#Roll()', function() {
     it('should return score of 10 if one pin striked each frame', function() {
         BowlingGame.Initialize();        
         for (i = 0; i < 10; i++){
             BowlingGame.Roll(1,0)
         }
         assert.equal(10, BowlingGame.GetScore());
+    });
+  });
+});
+
+describe('BowlingGame', function() {
+  describe('#Roll()', function() {
+    it('should double the score of the next frame if all pins striked in one frame (spare)', function() {
+        BowlingGame.Initialize();        
+        BowlingGame.Roll(1,9);
+        BowlingGame.Roll(1,0);
+        for (i = 2; i < 10; i++){
+            BowlingGame.Roll(0,0)
+        }
+        assert.equal(12, BowlingGame.GetScore());
+    });
+  });
+});
+
+describe('BowlingGame', function() {
+  describe('#Roll()', function() {
+    it('should double the next two strikes of the next frame if strike occurs and next frame is normal', function() {
+        BowlingGame.Initialize();        
+        BowlingGame.Roll(10,0);
+        BowlingGame.Roll(5,5);
+        for (i = 2; i < 10; i++){
+            BowlingGame.Roll(0,0)
+        }
+        assert.equal(30, BowlingGame.GetScore());
+    });
+  });
+});
+
+describe('BowlingGame', function() {
+  describe('#Roll()', function() {
+    it('should double the next two strikes of the next frame if strike occurs and next frame is normal', function() {
+        BowlingGame.Initialize();        
+        BowlingGame.Roll(10,0);
+        BowlingGame.Roll(10,0);
+        BowlingGame.Roll(5,5);
+        for (i = 3; i < 10; i++){
+            BowlingGame.Roll(0,0)
+        }
+        assert.equal(55, BowlingGame.GetScore());
     });
   });
 });
